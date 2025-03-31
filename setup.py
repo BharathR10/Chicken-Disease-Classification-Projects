b6a8d041ae861200364c54c7e18e575aba220173
Bharathharsha <bharathmuthu3517@gmail.com>
2025-03-31 23:18:00 +0530

    data ingestion completed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
+REPO_NAME = "Chicken-Disease-Classification--Projects"
 AUTHOR_USER_NAME = "BharathR10"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "bharathmuthu3517@gmail.com"
---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+# Define paths correctly
+CONFIG_FILE_PATH = Path("config/config.yaml")  # ✅ Correct
+PARAMS_FILE_PATH = Path("params.yaml")  # ✅ Correct
+
